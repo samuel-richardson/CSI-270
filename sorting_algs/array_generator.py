03dc38e6426d8d12f442e7ed845aef7d12c3aedf
samuel-richardson <samuel.richardson@mymail.champlain.edu>
2024-10-06 17:39:26 -0400

    Made scripts to generate sorting alg data

--- sorting_algs/array_generator.py
+++ sorting_algs/array_generator.py
@@ -2,7 +2,7 @@ import csv
 import random
 
 
-arrays = [['type', 'length', 'array']]
+arrays = [['length', 'random', 'sorted', 'reversed']]
 
 
 def add_array_range(x, y, size):
@@ -13,14 +13,12 @@ def add_array_range(x, y, size):
             array.append(random.randint(1, 10000))
             sorted_array = sorted(array)
             reversed_array = reversed(array)
-        arrays.append(['random', length, array])
-        arrays.append(['sorted', length, sorted_array])
-        arrays.append(['reversed', length, list(reversed_array)])
+        arrays.append([length, array, sorted_array, list(reversed_array)])
 
 
-add_array_range(10, 100, 5)
-add_array_range(1000, 1000, 5)
-add_array_range(10000, 10000, 5)
+add_array_range(10, 100, 2)
+add_array_range(1000, 1000, 2)
+add_array_range(10000, 10000, 2)
 
 
 with open('arrays.csv', 'w', newline='') as f:
---
+++ sorting_algs/time_tester.py
@@ -0,0 +1,53 @@
+from sort import *
+import csv
+import time
+
+with open('arrays.csv', newline='') as f:
+    reader = csv.reader(f)
+    arrays = list(reader)
+
+
+def test_algs(arrays):
+    results = [['Array Length',
+                'Bubble Random Execution Time',
+                'Bubble Sorted Execution Time',
+                'Bubble Reversed Execution Time',
+                'Selection Random Execution Time',
+                'Selection Sorted Execution Time',
+                'Selection Reversed Execution Time',
+                'Insertion Random Execution Time',
+                'Insertion Sorted Execution Time',
+                'Insertion Reversed Execution Time',
+                ]]
+    
+
+    def test_time(array, alg):
+        start_time = time.time()
+        alg(array)
+        end_time = time.time()
+        return round((end_time - start_time) * 1000, 2)
+
+    for array in arrays[1:]:
+        array_length = array[0]
+        random_array = [int(a) for a in array[1].strip('[').strip(']').split(', ')]
+        sorted_array = [int(a) for a in array[2].strip('[').strip(']').split(', ')]
+        reversed_array = [int(a) for a in array[3].strip('[').strip(']').split(', ')]
+        
+
+        results.append([array_length,
+                        test_time(random_array, bubble_sort),
+                        test_time(sorted_array, bubble_sort),
+                        test_time(reversed_array, bubble_sort),
+                        test_time(random_array, selection_sort),
+                        test_time(sorted_array, selection_sort),
+                        test_time(reversed_array, selection_sort),
+                        test_time(random_array, insertion_sort),
+                        test_time(sorted_array, insertion_sort),
+                        test_time(reversed_array, insertion_sort)
+                        ])
+    with open('results.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(results)
+
+
+test_algs(arrays)
